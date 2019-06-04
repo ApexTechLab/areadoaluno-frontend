79d08a90204f291215c33882198a6951738137e6
hotequil <joaohotequil@gmail.com>
2019-06-04 20:46:31 -0300

    Adiciona dependência react-datepicker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 import './index.css';
+import "react-datepicker/dist/react-datepicker.css";
 import 'bootstrap/dist/css/bootstrap.css';
-//import 'react-datepicker/dist/react-datepicker.css';
 
 ReactDOM.render(
   <App />,

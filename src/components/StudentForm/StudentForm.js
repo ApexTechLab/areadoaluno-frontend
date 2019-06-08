7c986c9f14df2fab2f66b3432dbe459fc432c1e9
hotequil <joaohotequil@gmail.com>
2019-06-08 18:00:25 -0300

    Ajuste nas validações que contém máscara

--- src/components/StudentForm/StudentForm.js
+++ src/components/StudentForm/StudentForm.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import { Formik, Field } from 'formik';
 import * as yup from 'yup';
 import { Form, Col, InputGroup, Button } from 'react-bootstrap';
-import DatePicker from "react-datepicker";
-import MaskedInput from 'react-text-mask';
 import Select from 'react-select';
-import MaskedTextInput from "react-text-mask";
+import InputMask from 'react-input-mask';
 
 class StudentForm extends Component {
   constructor(props) {
@@ -20,20 +18,21 @@ class StudentForm extends Component {
 
   render() {
     const schema = yup.object().shape({
-      name: yup.string().required().min(3),
+      name: yup.string().required().min(6),
       email: yup.string().required().email(),
-      birthDate: yup.string().required(),
-      phone: yup.string().required(),
-      cpf: yup.string().required(),
+      birthDate: yup.string().required().min(10),
+      phone: yup.string().required().min(16),
+      cpf: yup.string().required().min(14),
       classes: yup.array()
     });
 
+    //lembrar de tirar o options
     const options = [
       { value: '1', label: 'Formação Frontend Terças/Quintas' },
       { value: '2', label: 'Formação Java Sabados' },
       { value: '3', label: 'Formação C# segundao/quarta/sexta' }
     ];
-    
+
     return (
       <div className={"form"}>
         <Formik
@@ -59,11 +58,11 @@ class StudentForm extends Component {
             setFieldValue,
             setFieldTouched
           }) => (
-            <Form noValidate onSubmit={handleSubmit}>
+            <Form name="form" noValidate onSubmit={handleSubmit}>
               <Form.Row>
-                <Form.Group as={Col} md="6" controlId="validationFormik01">
+                <Form.Group as={Col} md="6" controlId="validationFormik01" >
                   <Form.Label>Nome Completo:</Form.Label>
-                  <Form.Control
+                  <Field
                     type="text"
                     name="name"
                     placeholder="Coloque o nome completo."
@@ -71,123 +70,92 @@ class StudentForm extends Component {
                     value={values.name}
                     onChange={handleChange}
                     isInvalid={touched.name && errors.name}
+                    className={
+                      errors.name && touched.name
+                        ? "text-input form-control is-invalid error"
+                      : "text-input form-control"
+                    }
                   />
+                  <span class="card-text text-small"><small class="text-muted">Campo obrigatório.</small></span>
                 </Form.Group>
 
                 <Form.Group as={Col} md="6" controlId="validationFormik01">
                   <Form.Label>E-mail:</Form.Label>
-                  <Form.Control
+                  <Field
                     type="email"
                     name="email"
                     placeholder="Coloque o e-mail."
                     value={values.email}
                     onChange={handleChange}
                     isInvalid={touched.email && errors.email}
+                    className={
+                      errors.email && touched.email
+                        ? "text-input form-control is-invalid error"
+                      : "text-input form-control"
+                    }
                   />
+                  <span class="card-text text-small"><small class="text-muted">Campo obrigatório.</small></span>
                 </Form.Group>
 
                 <Form.Group as={Col} md="6" controlId="validationFormik01">
                   <Form.Label>Telefone Móvel:</Form.Label>
-                  <Field
-                  name="phone"
-                  render={({ field }) => (
-                    <MaskedInput
-                      {...field}
-                      mask={[
-                        "(",
-                        /[0-9]/,
-                        /\d/,
-                        ")",
-                        " ",
-                        /\d/,
-                        " ",
-                        /\d/,
-                        /\d/,
-                        /\d/,
-                        /\d/,
-                        "-",
-                        /\d/,
-                        /\d/,
-                        /\d/,
-                        /\d/
-                      ]}
-                      id="phone"
-                      type="text"
-                      onChange={handleChange}
-                      placeholder="Coloque o telefone móvel"
-                      value={values.phone}
-                      onBlur={handleBlur}
-                      className={
-                        errors.phone && touched.phone
-                          ? "text-input form-control is-invalid error"
-                        : "text-input form-control"
-                      }
-                    />
-                  )}
-                />
+                  <InputMask id="phone"
+                    name="phone"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Coloque o telefone móvel"
+                    value={values.phone}
+                    onBlur={handleBlur}
+                    isInvalid={touched.phone && errors.phone}
+                    maskChar=""
+                    mask = "(99) 9 9999-9999"
+                    className={
+                      errors.phone && touched.phone
+                        ? "text-input form-control is-invalid error"
+                      : "text-input form-control"
+                    }/>
+                  <span class="card-text text-small"><small class="text-muted">Campo obrigatório.</small></span>
                 </Form.Group>
               
                 <Form.Group as={Col} md="6" controlId="validationFormik01">
                   <Form.Label>CPF:</Form.Label>
-                  <Field
-                  name="cpf"
-                  render={({ field }) => (
-                    <MaskedInput
-                      {...field}
-                      mask={[
-                        /[0-9]/,
-                        /\d/,
-                        /\d/,
-                        ".",
-                        /\d/,
-                        /\d/,
-                        /\d/,
-                        ".",
-                        /\d/,
-                        /\d/,
-                        /\d/,
-                        "-",
-                        /\d/,
-                        /\d/
-                      ]}
-                      id="cpf"
-                      type="text"
-                      value={values.cpf}
-                      onChange={handleChange}
-                      placeholder="Coloque o CPF."
-                      onBlur={handleBlur}
-                      className={
-                        errors.cpf && touched.cpf
-                          ? "text-input form-control is-invalid error"
-                        : "text-input form-control"
-                      }
-                    />
-                  )}
-                />
+                  <InputMask id="cpf"
+                    name="cpf"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Coloque o CPF."
+                    value={values.cpf}
+                    onBlur={handleBlur}
+                    isInvalid={touched.cpf && errors.cpf}
+                    maskChar=""
+                    mask = "999.999.999-99"
+                    className={
+                      errors.cpf && touched.cpf
+                        ? "text-input form-control is-invalid error"
+                      : "text-input form-control"
+                    }/>
+                  <span class="card-text text-small"><small class="text-muted">Campo obrigatório.</small></span>
                 </Form.Group>
 
                 <Form.Group as={Col} md="6" controlId="validationFormik01">
                   <Form.Label>Data de Nascimento:</Form.Label>
-                  <Field
-                  name="birthDate"
-                  render={({ field }) => (
-                    <MaskedTextInput
-                      {...field}
-                      mask={[/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/]}
-                      id="birthDate"
-                      type="text"
-                      placeholder="Coloque a data de nascimento."
-                      value={values.birthDate}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      className={
-                        errors.birthDate && touched.birthDate
-                          ? "text-input form-control is-invalid error"
-                        : "text-input form-control"
-                      }
-                    />
-                  )}
-                />
+                  <InputMask 
+                    id="birthDate"
+                    name="birthDate"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Coloque a data de nascimento."
+                    value={values.birthDate}
+                    onBlur={handleBlur}
+                    isInvalid={touched.birthDate && errors.birthDate}
+                    maskChar=""
+                    mask = "99/99/9999"
+                    className={
+                      errors.birthDate && touched.birthDate
+                        ? "text-input form-control is-invalid error"
+                      : "text-input form-control"
+                    }/>
+                  <span class="card-text text-small"><small class="text-muted">Campo obrigatório.</small></span>
                 </Form.Group>
 
                 <Form.Group as={Col} md="6" controlId="validationFormik01">
@@ -202,6 +170,7 @@ class StudentForm extends Component {
                     onBlur={handleBlur}
                     value={values.classes}
                   />
+                  <span class="card-text text-small"><small class="text-muted">Campo não obrigatório.</small></span>
                 </Form.Group>
 
                 <Button type="submit">Enviar Cadastro</Button>
@@ -213,7 +182,6 @@ class StudentForm extends Component {
       </div>
     )
   }
-
 }
 
 export default StudentForm;
